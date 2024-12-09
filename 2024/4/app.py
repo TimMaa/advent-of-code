e1e79c44073e046a1c8322cee5e62b7aa0b69b96
Tim Maassen <tim.maassen@roche.com>
2024-12-09 18:51:52 +0100

    2024 Day 4 - Still Trying
    A lot more condensed code now and probably a lot closer to the answer.
    Correct results for the examples so hard to debug now. :(

--- 2024/4/app.py
+++ 2024/4/app.py
@@ -1,83 +1,33 @@
-def searchWord(match_word, arr, dir, l, c, w_idx):
+directions = [
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+    (-1, 0),
+    (-1, 1)
+]
+
+x_params = {
+    (0, 1): [(1, 0), (-1, 1)],
+    (1, 1): [(1, -1), (0, 2)],
+    (1, 0): [(0, -1), (1, 1)],
+    (1, -1): [(-1, -1), (2, 0)],
+    (0, -1): [(-1, 0), (1, -1)],
+    (-1, -1): [(-1, 1), (0, -2)],
+    (-1, 0): [(0, 1), (-1, -1)],
+    (-1, 1): [(1, 1), (-2, 0)],
+}
+
+
+def search_word(match_word, arr, dir, start, w_idx):
     if w_idx == len(match_word):
         return True
 
-    # right
-    if dir == 0 and c != len(arr[0]) - 1:
-        c += 1
-    # right down
-    elif dir == 1 and c != len(arr[0]) - 1 and l != len(arr) - 1:
-        c += 1
-        l += 1
-    # down
-    elif dir == 2 and l != len(arr) - 1:
-        l += 1
-    # left down
-    elif dir == 3 and c != 0 and l != len(arr) - 1:
-        c -= 1
-        l += 1
-    # left
-    elif dir == 4 and c != 0:
-        c -= 1
-    # left up
-    elif dir == 5 and c != 0 and l != 0:
-        c -= 1
-        l -= 1
-    # up
-    elif dir == 6 and l != 0:
-        l -= 1
-    # right up
-    elif dir == 7 and c != len(arr[0]) - 1 and l != 0:
-        c += 1
-        l -= 1
+    new_l, new_c = map(sum, zip(start, dir))
 
-    if c > -1 and l > -1 and arr[l][c] == match_word[w_idx]:
-        return searchWord(match_word, arr, dir, l, c, w_idx+1)
-    else:
-        return False
-
-
-def searchX(match_word, arr, dir, l, c):
-    ls, le, cs, ce = None, None, None, None
-
-    # right
-    if dir == 0:
-        ls, le = l-1, l+1
-        cs, ce = c+1, c+1
-    # right down
-    elif dir == 1:
-        ls, le = l, l-2
-        cs, ce = c+2, c
-    # down
-    elif dir == 2:
-        ls, le = l+1, l+1
-        cs, ce = c+1, c-1
-    # left down
-    elif dir == 3:
-        ls, le = l-2, l
-        cs, ce = c, c-2
-    # left
-    elif dir == 4:
-        ls, le = l+1, l-1
-        cs, ce = c-1, c-1
-    # left up
-    elif dir == 5:
-        ls, le = l, l-2
-        cs, ce = c-2, c
-    # up
-    elif dir == 6:
-        ls, le = l-1, l-1
-        cs, ce = c-1, c+1
-    # right up
-    elif dir == 7:
-        ls, le = l, l+2
-        cs, ce = c+2, c
-
-    if ls in range(len(arr)) and cs in range(len(arr[0])) and le in range(len(arr)) and ce in range(len(arr[0])):
-        if arr[ls][cs] == match_word[0] and arr[le][ce] == match_word[2]:
-            return True
-
-    return False
+    return search_word(match_word, arr, dir, (new_l, new_c), w_idx+1) if new_l in range(0, len(arr)) and new_c in range(0, len(arr[0])) and arr[new_l][new_c] == match_word[w_idx] else False
 
 
 with open("./input.txt", "r") as input:
@@ -91,14 +41,18 @@ with open("./input.txt", "r") as input:
     for l_idx, line in enumerate(array):
         for c_idx, char in enumerate(line):
             if char == "X":
-                for dir in range(8):
-                    if searchWord("XMAS", array, dir, l_idx, c_idx, 1):
+                for dir in directions:
+                    if search_word("XMAS", array, dir, (l_idx, c_idx), 1):
                         cnt += 1
             if char == "M":
-                for dir in range(8):
-                    if searchWord("MAS", array, dir, l_idx, c_idx, 1):
-                        if searchX("MAS", array, dir, l_idx, c_idx):
-                            x_cnt += 1
+                for dir in directions:
+                    if search_word("MAS", array, dir, (l_idx, c_idx), 1):
+                        x_param = x_params[dir]
+                        new_l, new_c = tuple(
+                            map(sum, zip((l_idx, c_idx), x_param[1])))
+                        if new_l in range(0, len(array)) and new_c in range(0, len(array[0])) and array[new_l][new_c] == "M":
+                            if search_word("MAS", array, x_param[0], (new_l, new_c), 1):
+                                x_cnt += 1
 
     print(cnt)
     print(x_cnt)
