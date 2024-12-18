b6835c383b3b63f6851c843383659855463726c1
Tim Maassen <tim.maassen@roche.com>
2024-12-18 12:27:22 +0100

    2024 Day 14 - Part 2
    I did come back. No idea how the tree should look like, but for a picture robots had to be close.
    Played around with the neighbors value a bit until one clearly jumped out. Over 1000 it is.

--- 2024/14/app.py
+++ 2024/14/app.py
@@ -1,8 +1,6 @@
 robots = []
-els = []
 field_x = 101
 field_y = 103
-time = 100
 
 with open("./input.txt", "r") as input:
     for line in input:
@@ -12,18 +10,27 @@ with open("./input.txt", "r") as input:
             tuple(map(int, v[2:].split(",")))
         ])
 
-for rs, rv in robots:
-    ex, ey = (time*rv[0] + rs[0]) % field_x, (time*rv[1] + rs[1]) % field_y
-    els.append((ex, ey))
-
-
-field_x -= 1
-field_y -= 1
-
-q1 = sum([1 for lx, ly in els if lx < field_x/2 and ly < field_y/2])
-q2 = sum([1 for lx, ly in els if lx > field_x/2 and ly < field_y/2])
-q3 = sum([1 for lx, ly in els if lx < field_x/2 and ly > field_y/2])
-q4 = sum([1 for lx, ly in els if lx > field_x/2 and ly > field_y/2])
-
-
+els = [((100*rv[0] + rs[0]) % field_x, (100*rv[1] + rs[1]) % field_y)
+       for rs, rv in robots]
+q1 = sum([1 for lx, ly in els if lx < field_x//2 and ly < field_y//2])
+q2 = sum([1 for lx, ly in els if lx > field_x//2 and ly < field_y//2])
+q3 = sum([1 for lx, ly in els if lx < field_x//2 and ly > field_y//2])
+q4 = sum([1 for lx, ly in els if lx > field_x//2 and ly > field_y//2])
 print(q1 * q2 * q3 * q4)
+
+for t in range(0, field_x*field_y):
+    layout = set([((t*rv[0] + rs[0]) % field_x, (t*rv[1] + rs[1]) % field_y)
+                  for rs, rv in robots])
+    neighbors = 0
+    for robot in layout:
+        for d in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+          if tuple(map(sum, zip(robot, d))) in layout:
+              neighbors += 1
+
+    if neighbors > 1000:
+      for x in range(field_x):
+          for y in range(field_y):
+              print("*", end="") if (x, y) in layout else print(".", end="")
+          print("")
+      print("t:", t)
+      break
