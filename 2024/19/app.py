80e261402d857c116277b886d3f5c3a24d2f4869
Tim Maassen <tim.maassen@roche.com>
2024-12-19 16:24:47 +0100

    2024 Day 19 - Part 2

--- 2024/19/app.py
+++ 2024/19/app.py
@@ -1,34 +1,29 @@
-def match_pattern(line, patterns, failed_patterns):
-    if line in failed_patterns:
-        return failed_patterns
+patterns = []
+known_patterns = {'': 1}
 
-    if len(line) == 0:
-        return []
 
-    possible_patterns = [p for p in patterns if len(
-        p) <= len(line) and p.startswith(line[0:len(p)])]
+def match_pattern(design: str):
+    if design in known_patterns:
+        return known_patterns[design]
 
-    if len(possible_patterns) == 0:
-        failed_patterns.add(line)
-
-    # print(line, possible_patterns)
-    for p in possible_patterns:
-        result = match_pattern(line[len(p):], patterns, failed_patterns)
-        if len(result) == 0:
-            return []
-        else:
-            failed_patterns.add(line)
-
-    return failed_patterns
+    ways = sum(match_pattern(design[len(p):])
+               for p in patterns if design.startswith(p))
+    known_patterns[design] = ways
+    return ways
 
 
 with open("./input.txt", "r") as input:
     patterns = [p.strip() for p in input.readline().split(",")]
 
     input.readline()  # Skip empty line
+
     possible = 0
+    variations = 0
     for line in input:
-        result = match_pattern(line.strip(), patterns, set())
-        if len(result) == 0:
+        success = match_pattern(line.strip())
+        if success != 0:
             possible += 1
+        variations += success
+
     print(possible)
+    print(variations)
