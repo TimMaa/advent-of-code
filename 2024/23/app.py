ff12f0f2982b3fe6c1d7bc37e4ee23a3e51fe143
Tim Maassen <tim.maassen@roche.com>
2024-12-23 15:37:53 +0100

    2024 Day 23 - Small Code Improvements

--- 2024/23/app.py
+++ 2024/23/app.py
@@ -1,26 +1,21 @@
 sets = set()
-computers = set()
 comp_graph = {}
 
 with open("./input.txt", "r") as input:
     for line in input:
         a, b = line.strip().split("-")
         sets.add((a, b))
-        computers.add(a)
-        computers.add(b)
+        comp_graph[a] = comp_graph[a] + [b] if a in comp_graph else [b]
+        comp_graph[b] = comp_graph[b] + [a] if b in comp_graph else [a]
 
-for c in computers:
-    comp_graph[c] = set([t for d in [con for con in sets if c in con]
-                         for t in d if t != c])
-
-for i in range(3, len(computers)):
+for i in range(3, len(comp_graph)):
     longest_sets = [s for s in sets if len(s) == i-1]
     if len(longest_sets) == 0:
         # Part 2
         print(",".join(sorted(max(sets, key=len))))
         break
     for elements in longest_sets:
-        for cn in [c for c in computers if all(c in comp_graph[e] for e in elements)]:
+        for cn in [c for c in comp_graph if all(c in comp_graph[e] for e in elements)]:
             sets.add(tuple(sorted(elements + (cn,))))
     if i == 3:
         # Part 1
