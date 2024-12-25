200fdb0942e9f5a1f3900afadd2c2b52dfe32c35
Tim Maassen <tim.maassen@roche.com>
2024-12-25 15:07:08 +0100

    2024 Day 17 - Nope
    Tried to understand how the program works, but I just can't.
    This way it will find a result somewhere in a billion years.

--- 2024/17/app.py
+++ 2024/17/app.py
@@ -1,8 +1,8 @@
-a, b, c = None, None, None
+base_a, base_b, base_c = None, None, None
 instructions = []
 
 
-def interpret_combo(operand):
+def interpret_combo(operand, a, b, c):
     if operand < 4:
         return operand
     if operand == 4:
@@ -15,44 +15,60 @@ def interpret_combo(operand):
         return None
 
 
+def run_program(a):
+    b = 0
+    c = 0
+    pointer = 0
+
+    output = []
+
+    while pointer < len(instructions):
+        opcode = instructions[pointer]
+        literal = instructions[pointer+1]
+        combo = interpret_combo(literal, a, b, c)
+        if opcode == 0:
+            a = a // pow(2, combo)
+        elif opcode == 1:
+            b = b ^ literal
+        elif opcode == 2:
+            b = combo % 8
+        elif opcode == 3 and a != 0:
+            pointer = literal
+            continue
+        elif opcode == 4:
+            b = b ^ c
+        elif opcode == 5:
+            output.append(str(combo % 8))
+        elif opcode == 6:
+            b = a // pow(2, combo)
+        elif opcode == 7:
+            c = a // pow(2, combo)
+        pointer += 2
+    return output
+
+
 with open("./input.txt", "r") as input:
     for line in input:
         name, *values = line.strip().split(" ")
         if name == "Register":
             if values[0] == "A:":
-                a = int(values[1])
+                base_a = int(values[1])
             elif values[0] == "B:":
-                b = int(values[1])
+                base_b = int(values[1])
             elif values[0] == "C:":
-                c = int(values[1])
+                base_c = int(values[1])
         elif name == "Program:":
             instructions = [int(x) for x in values[0].split(",")]
 
-pointer = 0
-output = []
+print(",".join(run_program(base_a)))
 
-while pointer < len(instructions):
-    opcode = instructions[pointer]
-    lit = instructions[pointer+1]
-    comb = interpret_combo(lit)
+str_inst = list(map(str, instructions))
+base_a = pow(8, len(instructions)-1)
 
-    if opcode == 0:
-        a = a // pow(2, comb)
-    elif opcode == 1:
-        b = b ^ lit
-    elif opcode == 2:
-        b = comb % 8
-    elif opcode == 3 and a != 0:
-        pointer = lit
-        continue
-    elif opcode == 4:
-        b = b ^ c
-    elif opcode == 5:
-        output.append(str(comb % 8))
-    elif opcode == 6:
-        b = a // pow(2, comb)
-    elif opcode == 7:
-        c = a // pow(2, comb)
-    pointer += 2
-
-print(",".join(output))
+while base_a < pow(8, len(instructions))-1:
+    base_a += 1
+    result = run_program(base_a)
+    print(base_a, ";", result)
+    if result == str_inst:
+        print("Found:", base_a)
+        break
